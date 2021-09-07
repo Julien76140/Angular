08480d45f19a8c8bcba14dd4f3ccf0b64fde9eef
Julien76140 <julien.mallet.pro@gmail.com>
2021-09-07 14:22:23 +0200

    fix type date

--- src/app/mock-races.ts
+++ src/app/mock-races.ts
@@ -2,8 +2,8 @@ import { Race } from "./race";
 
 export const RACES :Race[]=[
 
-  {id_race: 1, location: 'ROUEN', date: '2021/09/08'},
+  {id_race: 1, location: 'ROUEN', date: new Date(),ponies:[]},
 
-  {id_race: 1, location: 'TOKYO', date: '2021/09/08'},
+  {id_race: 1, location: 'TOKYO', date: new Date(),ponies:[]},
 
 ];
--- src/app/race.ts
+++ src/app/race.ts
@@ -1,14 +1,18 @@
+import {Pony} from "./pony";
+
 export class Race {
 
   id_race: number;
   location: string;
-  date: string
+  date: Date;
+  ponies: Array<Pony>
 
-    constructor(id_race?:number,location?:string,date?:string) {
+    constructor(id_race?:number,location?:string,date?:Date) {
 
       this.id_race= id_race === undefined ? 0 : id_race;
       this.location= location === undefined ? "location" : location;
-      this.date= date === undefined ? "date" : date;
+      this.date= date === undefined ? new Date() : date;
+      this.ponies=[];
 
 }
 }
